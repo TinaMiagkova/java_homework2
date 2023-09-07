cedd251c9e5c4caac271efaa5438cfca1de8937c
Tina <valentinamiagkova@gmail.com>
2023-09-07 17:20:05 +0300

    addMainSolved

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 26_519;
+        int mileCost = 20;
+        int miles = ticketCost / mileCost;
+        System.out.println("Количество миль:" + miles);
+    }
 }
